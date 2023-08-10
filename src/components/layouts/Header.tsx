e944a1abab365eec5678ea8b60a52a0172ce6b98
Basam Serad <basamserad1223@gmail.com>
2023-08-10 16:42:02 +0800

    Landing Page and Header

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,5 +1,39 @@
-const Header = () => {
-  return <div>Header</div>;
-};
+"use client";
+import { useState } from "react";
+import logo from "/public/logo/merdaka.png";
+import Image from "next/image";
+import NavBarDesktop from "./NavBarDesktop";
+import NavBarMobile from "./NavBarMobile";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+
+function Header() {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+    });
+  };
+
+  return (
+    <header className="flex items-center justify-between p-4 fixed bg-white w-screen z-50 laptop:px-20">
+      <div className="flex gap-4 cursor-pointer" onClick={scrollToTop}>
+        <Image className="h-14 w-14 " src={logo} alt="merdaka logo" />
+        <p className="text-lg font-medium">
+          Merdaka Information
+          <br /> Teknologi
+        </p>
+      </div>
+      <NavBarDesktop />
+      <NavBarMobile
+        style={openMenu ? { display: "flex" } : { display: "none" }}
+        closeBtn={() => setOpenMenu(!openMenu)}
+      />
+      <div className="tablet:hidden" onClick={() => setOpenMenu(!openMenu)}>
+        <MenuOutlinedIcon style={{ height: "30px", width: "30px" }} />
+      </div>
+    </header>
+  );
+}
 
 export default Header;
--- src/components/layouts/NavBarMobile.tsx
+++ src/components/layouts/NavBarMobile.tsx
@@ -1,5 +1,42 @@
-const NavBarMobile = () => {
-  return <div>NavBarMobile</div>;
-};
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import logo from "/public/logo/merdaka.png";
+import Image from "next/image";
+
+function NavBarMobile({ style, closeBtn }: { style: any; closeBtn: any }) {
+  return (
+    <nav
+      style={style}
+      className="flex flex-col bg-black text-white w-screen h-screen absolute top-0 left-0"
+    >
+      <div className="flex p-4 items-center justify-between mb-8">
+        <div className="flex gap-4">
+          <Image className="h-14 w-14" src={logo} alt="merdaka logo" />
+          <p className="text-lg font-medium">
+            Merdaka Information
+            <br /> Teknologi
+          </p>
+        </div>
+        <CloseOutlinedIcon
+          style={{ height: "30px", width: "30px" }}
+          onClick={closeBtn}
+        />
+      </div>
+      <div className="flex flex-col text-4xl font-semibold text-right pr-5 gap-4">
+        <a onClick={closeBtn} href="#">
+          Home
+        </a>
+        <a onClick={closeBtn} href="#about-us">
+          About
+        </a>
+        <a onClick={closeBtn} href="#services">
+          Services
+        </a>
+        <a onClick={closeBtn} href="#contact-us">
+          Contact Us
+        </a>
+      </div>
+    </nav>
+  );
+}
 
 export default NavBarMobile;
