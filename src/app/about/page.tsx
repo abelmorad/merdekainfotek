842130800d465ef78d257570a3fba0c1540f7c87
Basam Serad <basamserad1223@gmail.com>
2023-08-12 16:46:24 +0800

    Apply Framer Motion

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,7 @@
+'use client'
 import TeamCard from '@/components/cards/TeamCard'
+import { motion } from 'framer-motion'
+import { fadeIn } from '@/util/variants'
 
 const page = () => {
   return (
@@ -8,7 +11,9 @@ const page = () => {
     >
       <section className="flex flex-col gap-10">
         <div className="flex-col">
-          <h2 className="capitalize mb-5 font-semibold text-4xl">about us</h2>
+          <motion.h2 className="capitalize mb-5 font-semibold text-4xl">
+            about us
+          </motion.h2>
           <p className="desktop:px-48">
             Merdeka Information Teknologi is a web development company based in
             the Philippines. We are a team of Muslim engineers specializing in
@@ -17,12 +22,24 @@ const page = () => {
         </div>
       </section>
       <div className="flex-col my-20">
-        <h2 className="capitalize mb-10 font-semibold text-4xl">
+        <motion.h2
+          variants={fadeIn('up', 0.3)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="capitalize mb-10 font-semibold text-4xl"
+        >
           meet our team
-        </h2>
-        <div className="flex flex-col items-center gap-5 tablet:grid tablet:grid-flow-col tablet:grid-rows-3 tablet:place-content-center laptop:grid-rows-2 desk">
+        </motion.h2>
+        <motion.div
+          variants={fadeIn('up', 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col items-center gap-5 tablet:grid tablet:grid-flow-col tablet:grid-rows-3 tablet:place-content-center laptop:grid-rows-2 desk"
+        >
           <TeamCard />
-        </div>
+        </motion.div>
       </div>
     </section>
   )
