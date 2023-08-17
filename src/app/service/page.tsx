70c11ed1542098a5e3622de57d4c17d59caaa07b
abelmorad <abelmorad97@gmail.com>
2023-08-18 06:16:02 +0800

    Style: adjust y padding

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -2,7 +2,7 @@ const page = () => {
   return (
     <section
       id="service"
-      className="flex flex-col pt-28 mobile:text-center mobile:px-4 tablet:px-16 desktop:px-44"
+      className="flex flex-col pt-36 pb-20 mobile:text-center mobile:px-4 tablet:px-16 desktop:px-44"
     >
       <h1>Services</h1>
     </section>
