609c6a25e43731f8334980b8a5c97fd70f44c5db
Sharhabeel B. Morad <abelmorad97@gmail.com>
2023-08-24 14:11:09 +0800

    Style: Fix responsive and team card responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,49 +5,46 @@ import { motion, useInView } from 'framer-motion'
 import { fadeIn } from '@/util/variants'
 import MissionVision from '@/components/MissionVision'
 
-
 const Page = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+  const ref = useRef(null)
+  const isInView = useInView(ref, { once: true })
 
   return (
     <section
       id="about"
-      className="flex flex-col pt-36 pb-20 px-4 text-center tablet:px-16 desktop:px-44"
+      className="flex flex-col pt-36 pb-20 px-4 text-center tablet:px-16 laptop:px-12 desktop:px-44"
     >
-      <div className="flex flex-col gap-3">
-        <div className="flex-col">
-          <h1 className="title">
-            about us
-          </h1>
-          <p className="sub_title">
-            Merdeka Information Teknologi is a web development company based in
-            the Philippines. We are a team of Muslim engineers specializing in
-            building web applications.
-          </p>
-        </div>
+      <div className="flex flex-col gap-3 mb-10">
+        <h1 className="title">about us</h1>
+        <p>
+          Merdeka Information Teknologi is a web development company based in
+          the Philippines. We are a team of Muslim engineers specializing in
+          building web applications.
+        </p>
+      </div>
+      <div className="flex flex-col gap-10">
         <MissionVision />
-        <div className="flex-col my-20">
-          <motion.h2
-            variants={fadeIn('up', 0.2)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="capitalize mb-10 font-semibold text-4xl"
-          >
-            meet our team
-          </motion.h2>
-          <motion.div
-            ref={ref}
-            className='grid place-items-center gap-8 sm:grid-cols-2 sm:px-16 laptop:grid-cols-3 desktop:px-36'
-            initial="hidden"
-            animate={isInView ? "show" : "hidden"}
-            exit="hidden"
-            variants={fadeIn('up', 0.3)}
-          >
-            <TeamCard />
-          </motion.div>
-        </div>
+      </div>
+      <div className="flex-col my-20">
+        <motion.h2
+          variants={fadeIn('up', 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="title"
+        >
+          meet our team
+        </motion.h2>
+        <motion.div
+          ref={ref}
+          className="flex flex-wrap place-content-center gap-8"
+          initial="hidden"
+          animate={isInView ? 'show' : 'hidden'}
+          exit="hidden"
+          variants={fadeIn('up', 0.3)}
+        >
+          <TeamCard />
+        </motion.div>
       </div>
     </section>
   )
