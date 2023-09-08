273e40a1f913a62832890b9f618493430e211863
Sharhabeel B. Morad <abelmorad97@gmail.com>
2023-09-09 06:04:50 +0800

    Fix: remove white space fix and fixed overflow of content to footer

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -5,17 +5,19 @@ const page = () => {
   return (
     <section
       id="service"
-      className="flex flex-col pt-36 pb-20 text-center px-4 tablet:px-16 desktop:px-44"
+      className="flex flex-col text-center px-4 tablet:px-16 desktop:px-44"
     >
-      <div className="flex flex-col mb-10 ">
-        <h2 className="title">Products & Services</h2>
-        <p className='text-gray-600'>
-          We&apos;re providing a very affordable cost for a high quality
-          services.
-        </p>
-      </div>
-      <div className="flex flex-col max-w-max gap-7 justify-center tablet:grid grid-cols-2">
-        <ServiceCard />
+      <div className='flex flex-col py-36 mb-36 h-screen'>
+        <div className="flex flex-col mb-10">
+          <h2 className="title">Products & Services</h2>
+          <p className='text-gray-600'>
+            We&apos;re providing a very affordable cost for a high quality
+            services.
+          </p>
+        </div>
+        <div className="flex flex-col max-w-max gap-7 justify-center tablet:grid grid-cols-2">
+          <ServiceCard />
+        </div>
       </div>
     </section>
   )
