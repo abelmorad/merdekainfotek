1ed4e66a0741b703b94af1c9c7332bae1066cdfa
Sharhabeel B. Morad <abelmorad97@gmail.com>
2023-09-14 09:14:21 +0800

    Update

--- src/components/cards/TeamCard.tsx
+++ src/components/cards/TeamCard.tsx
@@ -1,5 +1,4 @@
 import { team } from '@/constant'
-import Image from 'next/image'
 
 const TeamCard = () => {
   return (
@@ -9,12 +8,10 @@ const TeamCard = () => {
           className="flex flex-col place-content-center bg-white shadow-md items-center border border-gray-300 rounded-md h-72 w-60 px-5 py-10 text-center"
           key={data.id}
         >
-          <Image
-            src="/avatar/fullstack.png"
+          <img
+            src={data.image}
             alt={data.name}
-            className="mb-4"
-            width={128}
-            height={128}
+            className="h-36 w-36 mb-4 rounded-full object-cover border border-gray-700"
           />
           <p>{data.name}</p>
           <p>
